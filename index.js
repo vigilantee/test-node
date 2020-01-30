8e19a6f4caad6f822f96363e1ea65b622536c544
Abhishek <abhishek@Abhisheks-MacBook-Pro-2.local>
2020-01-30 22:40:54 +0530

    Completed the chat application

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 var express = require("express");
 var app = express();
 
+var MongoClient = require("mongodb").MongoClient;
+var url =
+  "mongodb+srv://vsm-superadmin:vsm123@vsm-6jj8t.mongodb.net/development?authSource=admin&replicaSet=vsm-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true";
+
+let dbCollection;
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("development");
+  dbCollection = dbo.collection("chatTest");
+});
+const loadChat = async name => {
+  var query = { name };
+  let result = await dbCollection.find(query).toArray();
+  return result;
+};
+const writeMsgtoDB = msgObj => {
+  dbCollection.insert(msgObj);
+};
+
 const http = require("http").Server(app);
 const io = require("socket.io")(http);
 
@@ -14,37 +33,33 @@ app.use(bodyParser.json());
 var port = process.env.PORT || 8080;
 var router = express.Router();
 
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 app.get("/", function(req, res) {
   res.render("index.ejs");
 });
 
-router.get("/messages", function(req, res) {
-  const messages = {
-    1: ["Hi whats up", "how are you"],
-    2: ["I love you"]
-  };
-  res.json({ messages });
+router.get("/messages", async function(req, res) {
+  const chat = await loadChat("Abhishek");
+  return res.json(chat);
 });
 
 router.post("/postMessage", function(req, res) {
-  //   const message =
-  //     "I am good. Can you write my notes for Thermodynamics? I got some commitments.";
-  //   const body = {
-  //     id: 3,
-  //     message
-  //   };
+  writeMsgtoDB(req.body);
   res.json(req.body);
 });
 app.use("/api", router);
 
 io.sockets.on("connection", function(socket) {
-  socket.on("username", function(username) {
+  socket.on("username", async function(username) {
     socket.username = username;
     io.emit("is_online", "🔵 <i>" + socket.username + " join the chat..</i>");
+    const chat = await loadChat(socket.username);
+    chat.map(({message})=>{
+      io.emit("add_message_to_ui", "<i>" + socket.username + "\t" + message + "</i>");
+    })
   });
-  socket.on("chat_message", function(msg) {
-    console.log("msg we got from request ......", msg);
+  socket.on("chat_message", async function(msg) {
+    io.emit("add_message_to_ui", "<i>" + socket.username + "\t" + msg + "</i>");
+    await writeMsgtoDB({ name: socket.username, message: msg });
   });
 });
 
